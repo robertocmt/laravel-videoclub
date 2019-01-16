361e1471c018ebe7782e4c2c965fc1f0220d5690
robertocmt <1smr_robertocarlos@iesiliberis.com>
2019-01-16 11:11:15 +0100

    Update show.blade.php

--- videoclub/resources/views/catalog/show.blade.php
+++ videoclub/resources/views/catalog/show.blade.php
@@ -6,7 +6,32 @@
 <body>
 	@extends('layouts.master')
 	@section('content')
-		<h1>Vista detalle película {{$id}}</h1>
+		<div class="row">
+			<div class="col-sm-4">
+				<img src="{{$arrayPeliculas['poster']}}" width="100%" />
+			</div>
+			<div class="col-sm-8">
+				<h3>{{$arrayPeliculas['title']}}</h3>
+				<h5>Año: {{$arrayPeliculas['year']}}</h5>
+				<h5>Director: {{$arrayPeliculas['director']}}</h5>
+				<p><b>Resumen:</b> {{$arrayPeliculas['synopsis']}}</p>
+				<p>
+					<b>Estado: </b>
+					@if($arrayPeliculas['rented'] == false)
+					Película disponible.
+					@else
+					Película actualmente alquilada.
+					@endif
+				</p>
+				@if($arrayPeliculas['rented'] == false)
+				<button type="button" class="btn btn-primary">Alquilar pelicula</button>
+				@else
+				<button type="button" class="btn btn-danger">Devolver pelicula</button>
+				@endif
+				<a href="{{ url('/catalog/edit/' . $id_pelicula ) }}" class="btn btn-warning">Editar pelicula</a>
+				<a href="{{ url('/catalog' ) }}" class="btn btn-light">Volver al listado</a>
+			</div>
+		</div>
 	@stop
 </body>
 </html>
