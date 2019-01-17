c41cec801af40303e455fecbe3616b908406aef8
robertocmt <1smr_robertocarlos@iesiliberis.com>
2019-01-17 10:23:55 +0100

    Update show.blade.php

--- videoclub/resources/views/catalog/show.blade.php
+++ videoclub/resources/views/catalog/show.blade.php
@@ -8,27 +8,27 @@
 	@section('content')
 		<div class="row">
 			<div class="col-sm-4">
-				<img src="{{$arrayPeliculas['poster']}}" width="100%" />
+				<img src="{{$pelicula->poster}}" width="100%" />
 			</div>
 			<div class="col-sm-8">
-				<h3>{{$arrayPeliculas['title']}}</h3>
-				<h5>Año: {{$arrayPeliculas['year']}}</h5>
-				<h5>Director: {{$arrayPeliculas['director']}}</h5>
-				<p><b>Resumen:</b> {{$arrayPeliculas['synopsis']}}</p>
+				<h3>{{$pelicula->title}}</h3>
+				<h5>Año: {{$pelicula->year}}</h5>
+				<h5>Director: {{$pelicula->director}}</h5>
+				<p><b>Resumen:</b> {{$pelicula->synopsis}}</p>
 				<p>
 					<b>Estado: </b>
-					@if($arrayPeliculas['rented'] == false)
+					@if($pelicula->rented == false)
 					Película disponible.
 					@else
 					Película actualmente alquilada.
 					@endif
 				</p>
-				@if($arrayPeliculas['rented'] == false)
+				@if($pelicula->rented == false)
 				<button type="button" class="btn btn-primary">Alquilar pelicula</button>
 				@else
 				<button type="button" class="btn btn-danger">Devolver pelicula</button>
 				@endif
-				<a href="{{ url('/catalog/edit/' . $id_pelicula ) }}" class="btn btn-warning">Editar pelicula</a>
+				<a href="{{ url('/catalog/edit/' . $pelicula->id ) }}" class="btn btn-warning">Editar pelicula</a>
 				<a href="{{ url('/catalog' ) }}" class="btn btn-light">Volver al listado</a>
 			</div>
 		</div>
