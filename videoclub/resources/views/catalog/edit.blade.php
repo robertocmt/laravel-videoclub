5ab1481d61fc9cdfb752024fcfb446618c106110
robertocmt <1smr_robertocarlos@iesiliberis.com>
2019-01-17 10:23:48 +0100

    Update edit.blade.php

--- videoclub/resources/views/catalog/edit.blade.php
+++ videoclub/resources/views/catalog/edit.blade.php
@@ -21,30 +21,32 @@
 
             <div class="form-group">
                <label for="title">Título</label>
-               <input type="text" name="title" id="title" class="form-control">
+               <input type="text" name="title" id="title" class="form-control" value="{{$pelicula->title}}">
             </div>
 
             <div class="form-group">
                {{-- TODO: Completa el input para el año --}}
                <label for="title">Año</label>
-               <input type="text" name="year" id="year" class="form-control">
+               <input type="text" name="year" id="year" class="form-control" value="{{$pelicula->year}}">
             </div>
 
             <div class="form-group">
                {{-- TODO: Completa el input para el director --}}
                <label for="title">Director</label>
-               <input type="text" name="director" id="director" class="form-control">
+               <input type="text" name="director" id="director" class="form-control" value="{{$pelicula->director}}">
             </div>
 
             <div class="form-group">
                {{-- TODO: Completa el input para el poster --}}
                <label for="title">Poster</label>
-               <input type="text" name="poster" id="poster" class="form-control">
+               <input type="text" name="poster" id="poster" class="form-control" value="{{$pelicula->poster}}">
             </div>
 
             <div class="form-group">
                <label for="synopsis">Resumen</label>
-               <textarea name="synopsis" id="synopsis" class="form-control" rows="3"></textarea>
+               <textarea name="synopsis" id="synopsis" class="form-control" rows="3">
+               	value="{{$pelicula->synopsis}}"
+               </textarea>
             </div>
 
             <div class="form-group text-center">
