13f3baac449d6b3278a99b3d86375ee2b60bec34
robertocmt <1smr_robertocarlos@iesiliberis.com>
2019-01-23 18:31:09 +0100

    Update HomeController.php

--- videoclub/app/Http/Controllers/HomeController.php
+++ videoclub/app/Http/Controllers/HomeController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
     public function getHome()
     {
-    	return redirect()->action('CatalogController@getIndex');
+        if (Auth::check()) {
+            return redirect()->action('CatalogController@getIndex');
+        } else {
+            return view('auth.login');
+        }
     }
 }
