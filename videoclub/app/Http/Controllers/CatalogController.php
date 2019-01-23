72c48c5ba451d8811ce6954991b76b4365a10d46
robertocmt <1smr_robertocarlos@iesiliberis.com>
2019-01-23 18:31:07 +0100

    Update CatalogController.php

--- videoclub/app/Http/Controllers/CatalogController.php
+++ videoclub/app/Http/Controllers/CatalogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Movie;
+use Illuminate\Support\Facades\Auth;
 
 class CatalogController extends Controller
 {
@@ -25,5 +26,27 @@ class CatalogController extends Controller
     public function getEdit($id)
     {
     	return view('catalog.edit', array('pelicula'=>Movie::findOrFail($id)));
+    }
+
+    public function postCreate(Request $request)
+    {
+    	$pelicula = new Movie();
+    	$pelicula->title = $request->get('title');
+    	$pelicula->year = $request->get('year');
+    	$pelicula->director = $request->get('director');
+    	$pelicula->poster = $request->get('poster');
+    	$pelicula->synopsis = $request->get('synopsis');
+    	$pelicula->save();
+    	return redirect('/catalog');
+    }
+    public function putEdit(Request $request, $id){
+    	$pelicula = Movie::findOrFail($id);
+    	$pelicula->title = $request->get('title');
+    	$pelicula->year = $request->get('year');
+    	$pelicula->director = $request->get('director');
+    	$pelicula->poster = $request->get('poster');
+    	$pelicula->synopsis = $request->get('synopsis');
+    	$pelicula->save();
+    	return redirect('/catalog/show/'.$id);
     }
 }
